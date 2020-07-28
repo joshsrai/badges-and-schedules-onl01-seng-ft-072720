90bd57a9c1596330c10d5f76b8f2603509cb6724
Josh Raiborde <joshsrai@yahoo.com>
2020-07-28 17:28:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker(name)
    "Hello, my name is #{name}."
 end
 
-def batch_badge_creator["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def batch_badge_creator ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   counter = 1
   batch_badge_creator.each do |speakers| puts "Hello, my name is #{speakers}."}
   counter += 1
