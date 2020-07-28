4989fa8bed63a75e12e0a95d8dff71196a5814d8
Josh Raiborde <joshsrai@yahoo.com>
2020-07-28 17:21:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,3 +6,14 @@ end
 def batch_badge_creator
   
 end
+
+let(:attendees) {["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]}
+  let(:badges) {[
+                  "Hello, my name is Edsger.",
+                  "Hello, my name is Ada.",
+                  "Hello, my name is Charles.",
+                  "Hello, my name is Alan.",
+                  "Hello, my name is Grace.",
+                  "Hello, my name is Linus.",
+                  "Hello, my name is Matz."
+               ]}
