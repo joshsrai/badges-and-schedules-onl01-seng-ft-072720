fb6aa482595129830fe544b2d4bcff0784c6c6fe
Josh Raiborde <joshsrai@yahoo.com>
2020-07-28 17:27:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,18 +4,5 @@ def badge_maker(name)
 end
 
 def batch_badge_creator["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  batch_badge_creator.each{|speakers| puts 
-  
-  
+  batch_badge_creator.each{|speakers| puts "Hello, my name is #{speakers}."}
 end
-
-let(:attendees) {}
-  let(:badges) {[
-                  "Hello, my name is Edsger.",
-                  "Hello, my name is Ada.",
-                  "Hello, my name is Charles.",
-                  "Hello, my name is Alan.",
-                  "Hello, my name is Grace.",
-                  "Hello, my name is Linus.",
-                  "Hello, my name is Matz."
-               ]}
